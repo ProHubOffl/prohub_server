3670d6deba1c412e7eae652a3e79bfe696801643
Thiluxan <thiluxan@hotmail.co.uk>
2022-01-03 21:14:12 +0530

    Initialized ProHub Server

---
+++ src/main/java/com/epicwin/prohub/ProhubApplication.java
@@ -0,0 +1,13 @@
+package com.epicwin.prohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProhubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epicwin/prohub/ProhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicwin.prohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
