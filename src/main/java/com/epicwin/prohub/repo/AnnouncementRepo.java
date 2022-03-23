de6434c0f854591b675cc3177b05699ec641ca65
Sivanujan1998 <ssivanujan1998@gmail.com>
2022-03-23 23:24:32 +0530

    Unwanted import has been removed

--- src/main/java/com/epicwin/prohub/repo/AnnouncementRepo.java
+++ src/main/java/com/epicwin/prohub/repo/AnnouncementRepo.java
@@ -2,7 +2,6 @@ package com.epicwin.prohub.repo;
 
 import com.epicwin.prohub.model.announcement.Announcement;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
